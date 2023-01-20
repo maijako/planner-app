6e43cea2051592cb681e3add06ac313f726457bc
ensignjava <109529105+maijako@users.noreply.github.com>
2023-01-20 17:46:21 +0000

    added function to check if the hour block is in past, present or future

--- assets/index.js
+++ assets/index.js
@@ -6,43 +6,42 @@ var saveBtn = $(".saveBtn");
 
 //Variables to get planer text area blocks by hour
     var nineAM = document.getElementById("9");
-    var tenAM = $("#10");
-    var elevenAM = $("#11");
-    var twelvePM = $("#12");
-    var onePM = $("#13");
-    var twoPM= $("#14");
-    var threePM = $("#15");
-    var fourPM = $("#16");
-    var fivePM = $("#17");
-    
+    var tenAM = document.getElementById("10");
+    var elevenAM = document.getElementById("11");
+    var twelvePM = document.getElementById("12");
+    var onePM = document.getElementById("13");
+    var twoPM= document.getElementById("14");
+    var threePM = document.getElementById("15");
+    var fourPM = document.getElementById("16");
+    var fivePM = document.getElementById("17");
+    var hourTextarea = [nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM];    
 
 //Variables to store hour blocks as numbers
-var nine = parseInt(nineAM.dataset.indexNumber);
-var ten = parseInt(tenAM.dataset.indexNumber);
-var eleven = parseInt(elevenAM.dataset.indexNumber);
-var twelve = parseInt(twelvePM.dataset.indexNumber);
-var one = parseInt(onePM.dataset.indexNumber);
-var two = parseInt(twoPM.dataset.indexNumber);
-var three = parseInt(threePM.dataset.indexNumber);
-var four = parseInt(fourPM.dataset.indexNumber);
-var five = parseInt(fivePM.dataset.indexNumber);
-var hourBlocks = [nine, ten, eleven, twelve, one, two, three, four, five];
-// //The current date in the jumbotron
-// $("#currentDay").text(currentDay);
+    var nine = parseInt(nineAM.dataset.indexNumber);
+    var ten = parseInt(tenAM.dataset.indexNumber);
+    var eleven = parseInt(elevenAM.dataset.indexNumber);
+    var twelve = parseInt(twelvePM.dataset.indexNumber);
+    var one = parseInt(onePM.dataset.indexNumber);
+    var two = parseInt(twoPM.dataset.indexNumber);
+    var three = parseInt(threePM.dataset.indexNumber);
+    var four = parseInt(fourPM.dataset.indexNumber);
+    var five = parseInt(fivePM.dataset.indexNumber);
+    var hourBlocks = [nine, ten, eleven, twelve, one, two, three, four, five];
 
-// //check if planner hour blocks are in the present or the past
-// function checkTime () {
-//     for (var i = 0; i<hourBlocks.length; i++) {
-//         if (hourBlocks[i] < currentHour){
-//             hourBlocks[i].addClass("past");
-//         } else if (hourBlocks[i] > currentHour){
-//             hourBlocks[i].addClass("future");
-//         } else {
-//             hourBlocks[i].addClass("present");
-//         }
-//     }
-// }
+//The current date in the jumbotron
+$("#currentDay").text(currentDay);
 
-// checkTime();
+//check if planner hour blocks are in the present or the past
+function checkTime () {
+    for (var i = 0; i<hourBlocks.length; i++) {
+        if (hourBlocks[i] < currentHour){
+            $(hourTextarea[i]).addClass("past");
+        } else if (hourBlocks[i] > currentHour){
+            $(hourTextarea[i]).addClass("future");
+        } else {
+            $(hourTextarea[i]).addClass("present");
+        }
+    }
+}
+checkTime();
 
-console.log(typeof(nineAM.dataset.indexNumber));
