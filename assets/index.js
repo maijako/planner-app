0f4f00d04f783e5ebbebb7e70920564a851fdae0
ensignjava <109529105+maijako@users.noreply.github.com>
2023-01-20 16:52:38 +0000

    added time blocks to html and set variables to get the ids of each one

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,27 @@
+//Set variables
+var currentDay = moment().format('dddd, DD-MMM-YYYY HH:MM A');
+var currentHour = moment().format("H");
+var textArea = $("<textarea>");
+var saveBtn = $(".saveBtn");
+
+//Variables to get planer hour blocks
+    var nineAM = $("#9");
+    var tenAM = $("#10");
+    var elevenAM = $("#11");
+    var twelvePM = $("#12");
+    var onePM = $("#13");
+    var twoPM= $("#14");
+    var threePM = $("#15");
+    var fourPM = $("#16");
+    var fivePM = $("#17");
+
+
 //The current date in the jumbotron
-var currentDay = moment().format('dddd, DD-MMM-YYYY');
 $("#currentDay").text(currentDay);
+
+//check if planner hour blocks are in the present or the past
+function checkTime () {
+    
+
+}
 
