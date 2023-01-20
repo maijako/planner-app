49fcbda3c262205a7dc9a50f080a6948f0b98655
ensignjava <109529105+maijako@users.noreply.github.com>
2023-01-20 17:35:37 +0000

    added data index to each text area and stored it in a variable

--- assets/index.js
+++ assets/index.js
@@ -4,8 +4,8 @@ var currentHour = moment().format("H");
 var textArea = $("<textarea>");
 var saveBtn = $(".saveBtn");
 
-//Variables to get planer hour blocks
-    var nineAM = $("#9");
+//Variables to get planer text area blocks by hour
+    var nineAM = document.getElementById("9");
     var tenAM = $("#10");
     var elevenAM = $("#11");
     var twelvePM = $("#12");
@@ -14,14 +14,35 @@ var saveBtn = $(".saveBtn");
     var threePM = $("#15");
     var fourPM = $("#16");
     var fivePM = $("#17");
-
-
-//The current date in the jumbotron
-$("#currentDay").text(currentDay);
-
-//check if planner hour blocks are in the present or the past
-function checkTime () {
     
 
-}
+//Variables to store hour blocks as numbers
+var nine = parseInt(nineAM.dataset.indexNumber);
+var ten = parseInt(tenAM.dataset.indexNumber);
+var eleven = parseInt(elevenAM.dataset.indexNumber);
+var twelve = parseInt(twelvePM.dataset.indexNumber);
+var one = parseInt(onePM.dataset.indexNumber);
+var two = parseInt(twoPM.dataset.indexNumber);
+var three = parseInt(threePM.dataset.indexNumber);
+var four = parseInt(fourPM.dataset.indexNumber);
+var five = parseInt(fivePM.dataset.indexNumber);
+var hourBlocks = [nine, ten, eleven, twelve, one, two, three, four, five];
+// //The current date in the jumbotron
+// $("#currentDay").text(currentDay);
 
+// //check if planner hour blocks are in the present or the past
+// function checkTime () {
+//     for (var i = 0; i<hourBlocks.length; i++) {
+//         if (hourBlocks[i] < currentHour){
+//             hourBlocks[i].addClass("past");
+//         } else if (hourBlocks[i] > currentHour){
+//             hourBlocks[i].addClass("future");
+//         } else {
+//             hourBlocks[i].addClass("present");
+//         }
+//     }
+// }
+
+// checkTime();
+
+console.log(typeof(nineAM.dataset.indexNumber));
