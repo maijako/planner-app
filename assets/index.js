cca0f2189c0f1bbaf1085c7a41bd6b38a4ade4c9
ensignjava <109529105+maijako@users.noreply.github.com>
2023-01-23 08:18:13 +0000

    tidied up the formatting

--- assets/index.js
+++ assets/index.js
@@ -3,26 +3,26 @@ var currentDay = moment().format('dddd, MMMM Do');
 var currentHour = moment().format("H");
 
 //Variables to get planer text area blocks by hour
-    var nineAM = $("#9");
-    var tenAM = $("#10");
-    var elevenAM = $("#11");
-    var twelvePM = $("#12");
-    var onePM = $("#13");
-    var twoPM = $("#14");
-    var threePM = $("#15");
-    var fourPM = $("#16");
-    var fivePM = $("#17")
-var hourTextarea = [nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM];    
+var nineAM = $("#9");
+var tenAM = $("#10");
+var elevenAM = $("#11");
+var twelvePM = $("#12");
+var onePM = $("#13");
+var twoPM = $("#14");
+var threePM = $("#15");
+var fourPM = $("#16");
+var fivePM = $("#17")
+var hourTextarea = [nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM];
 
 //Display the current date in the jumbotron
 $("#currentDay").text(currentDay);
 
-function checkTime () {
-    for (var i = 0; i<hourTextarea.length; i++) {
+function checkTime() {
+    for (var i = 0; i < hourTextarea.length; i++) {
         var textNumber = parseInt(hourTextarea[i].attr("id"));
-        if (textNumber < currentHour){
+        if (textNumber < currentHour) {
             (hourTextarea[i]).addClass("past");
-        } else if (textNumber > currentHour){
+        } else if (textNumber > currentHour) {
             (hourTextarea[i]).addClass("future");
         } else {
             (hourTextarea[i]).addClass("present");
@@ -30,20 +30,19 @@ function checkTime () {
     }
 }
 checkTime();
-console.log(hourTextarea[0].class())
 
-    //Onclick event to save user input in local storage
-    $(".saveBtn").on("click", function (event){
-        event.preventDefault();
-        // Save the value of the textarea to sessionStorage
-        var textAreaId = $(this).prev().attr("id");
-        localStorage.setItem(textAreaId, $(this).prev().val());
-    });
+//Onclick event to save user input in local storage
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    // Save the value of the textarea to sessionStorage
+    var textAreaId = $(this).prev().attr("id");
+    localStorage.setItem(textAreaId, $(this).prev().val());
+});
 
-    //when the document is ready, load the values from sessionStorage
-    $(".description").each(function(){
-        $(this).val(localStorage.getItem($(this).attr("id")));
-    });
+//when the document is ready, load the values from sessionStorage
+$(".description").each(function () {
+    $(this).val(localStorage.getItem($(this).attr("id")));
+});
 
 
 
